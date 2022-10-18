a87cf17fcca58388b49ea7ad3bebe94e6e543289
Juliana-344 <chinwendujuliana344@gmail.com>
2022-10-18 08:19:25 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
+#include <unistid.h>
 #include "main.h"
 
 /**
  * main - print _putchar
  * is written by bwave
- * Return: 0
+ * Return: 0.
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n')
-return (0);
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
 }
+
